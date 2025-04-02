07baa344646da1e781d7f7c5b94e9e12fe01b683
rotanpuolikas <tuukka.alatalo@gmail.com>
2025-04-02 10:09:24 +0300

    ei toimi mut tempcommit, vaihan pc:lle ja korjaan myöhemmin

--- js/peli1.js
+++ js/peli1.js
@@ -1,13 +1,58 @@
 // kirjotuspeli...
 let viimeksi = null;
 let kirjaimet = "abcdefghijklmnopqrstuvwxysåäö "
+let pooh = null
 
+//texturl = "https://raw.githubusercontent.com/rotanpuolikas/webprojekti13/refs/heads/main/resources/pooh.txt"
+texturl = "./resources/pooh.txt"
+
+/*
+function getpooh(texturl){
+    return fetch(texturl).then(response => {
+        if(!response.ok){
+            throw new Error("HTTP error " + response.status)
+        }
+    })
+}
+*/
+
+function writePooh(pooh){
+    console.log(pooh)
+}
+/*
+async function getPooh(texturl){
+    try{
+        const response = await fetch(texturl)
+        if(!response.ok){
+            throw new Error("Response status: " + response.status)
+        }
+        const text = await response.text()
+        console.log(text)
+        return text
+    }
+    catch(error){
+        console.error(error.message)
+    }
+}
+*/
+
+async function getPooh(texturl){
+    let data = await fetch(texturl)
+        .then(value =>{
+            return value
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+pooh = getPooh(texturl)
 
 window.addEventListener("keydown", function (nappi) {
     if (nappi.defaultPrevented){
         return
     }
-
+    writePooh(pooh)
     if(kirjaimet.includes(nappi.key)){
         viimeksi = nappi.key
         kirjota(viimeksi, null)
