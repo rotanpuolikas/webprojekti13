1e9a1bbcddedd5f881a30c3e6034cfa033ffd1d1
rotanpuolikas <tuukka.alatalo@gmail.com>
2025-04-14 17:11:08 +0300

    tehtiin pelistä pelattava

--- js/peli1.js
+++ js/peli1.js
@@ -1,58 +1,61 @@
 // kirjotuspeli...
 let viimeksi = null;
-let kirjaimet = "abcdefghijklmnopqrstuvwxysåäö "
-let pooh = null
+let kirjaimet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖabcdefghijklmnopqrstuvwxysåäö ,."
+let pooh = ""
+let globalpooh = "" //välttelin typerää return ongelmaa tekemällä tästä globaalin muuttujan... ei ole optimi mutta tällä pärjätään
+
+let kohta = -1
 
 //texturl = "https://raw.githubusercontent.com/rotanpuolikas/webprojekti13/refs/heads/main/resources/pooh.txt"
 texturl = "./resources/pooh.txt"
 
-/*
-function getpooh(texturl){
-    return fetch(texturl).then(response => {
-        if(!response.ok){
-            throw new Error("HTTP error " + response.status)
-        }
-    })
-}
-*/
 
-function writePooh(pooh){
-    console.log(pooh)
-}
-/*
-async function getPooh(texturl){
-    try{
-        const response = await fetch(texturl)
-        if(!response.ok){
-            throw new Error("Response status: " + response.status)
-        }
-        const text = await response.text()
-        console.log(text)
-        return text
-    }
-    catch(error){
-        console.error(error.message)
-    }
-}
-*/
-
-async function getPooh(texturl){
-    let data = await fetch(texturl)
-        .then(value =>{
-            return value
+async function getPooh(texturl){ //haetaan paikallisesta tekstitiedostosta tuo data
+    await fetch(texturl)
+        .then(response => response.text())
+        .then(text => {
+            globalpooh = text
+            writePooh(globalpooh)
         })
         .catch(error => {
             console.error(error)
         })
 }
 
-pooh = getPooh(texturl)
+getPooh(texturl)
+
+
+
+/*
+globalpooh = "testi testi" //testi :)
+writePooh(globalpooh)
+*/
+
+
+function writePooh(pooh){
+    /*
+    teksti = document.getElementById("field")
+    teksti.innerHTML = pooh
+    */
+    tekstialue = document.getElementById("field")
+    tekstialue.textContent = null
+
+    let index = 0
+    //ähhh se piti tehä tälleen et jaetaan kaikki charit omiin spaneihin, muuten ei onnistu yksittäisten merkkien värjääminen, samalla laitetaan niille kaikille index attribute et voiaan trackata missä mennään
+    pooh.split('').forEach(char => {
+        let merkki = document.createElement("span")
+        merkki.innerText = char
+        merkki.setAttribute("index", index)
+        tekstialue.appendChild(merkki)
+        index += 1
+    })
+}
+
 
 window.addEventListener("keydown", function (nappi) {
     if (nappi.defaultPrevented){
-        return
+        return //idk dokumentaatiossa sanottiin et tää pitää olla, en tiiä mitä tekee mut varmaan tärkee
     }
-    writePooh(pooh)
     if(kirjaimet.includes(nappi.key)){
         viimeksi = nappi.key
         kirjota(viimeksi, null)
@@ -61,26 +64,39 @@ window.addEventListener("keydown", function (nappi) {
     else if(nappi.key == "Backspace"){
         kirjota(null, "Backspace")
     }
-    else{
+    else{ // jos tapahtuu jotain outoa nii ei tehä mitään
         viimeksi = null
         return viimeksi
     }
 })
 
-function kirjota(viimeksi, modify){
+function kirjota(viimeksi, modify){ // en tiiä miks päätin tehä tän kahella eri inputilla mut i quess selkeempi?
     teksti = document.getElementById("teksti")
-    if(modify == null){
-        uusteksti = teksti.innerHTML
-        uusteksti += viimeksi
-        teksti.innerHTML = uusteksti
-    }
-    else if(modify == "Backspace"){
-        let sis = teksti.innerHTML
-        pituus = sis.length
-        if(pituus > 0){
-        let minus1 = sis.slice(0, -1)
-        teksti.innerHTML = minus1
+    harjoitus = globalpooh.split('') //tätä ei tarvis joka kerta tehä uusiksi mut ei se kyl performanceakaan haittaa nii sama olla
+    
+    if(modify == null){ // jos kirjain
+        if(kohta < globalpooh.length - 1){
+            kohta += 1
+            oikeemerkki = harjoitus[kohta]
+            merkkiclass = document.querySelector("[index=" + CSS.escape(kohta) + "]") //tässä se maaginen index tulee hyötyyn
+            if(viimeksi === oikeemerkki){ //merkki oikein, väri vihreeksi
+                merkkiclass.classList.add("correct")
+                merkkiclass.classList.remove("incorrect")
+            }
+            else if(viimeksi != oikeemerkki){ // merkki väärin, väri punaseks
+                merkkiclass.classList.add("incorrect")
+                merkkiclass.classList.remove("correct")
+            }
         }
+    }
+    else if(modify == "Backspace"){ // jos backspace
+        merkkiclass = document.querySelector("[index=" + CSS.escape(kohta) + "]")
+        merkkiclass.classList.remove("correct") // poistetaan kaikki värit siit current merkistä ja siirrytää taaksepäi
+        merkkiclass.classList.remove("incorrect")
+        if(kohta > -1){
+            kohta -= 1
+        }
+
     }
     return
 }
