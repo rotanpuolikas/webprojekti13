e3841430d335c543838fed8664d09ab1b757b02d
rotanpuolikas <tuukka.alatalo@gmail.com>
2025-04-24 13:07:24 +0300

    sessionstorage testaus toimii

---
+++ js/pisteet.js
@@ -0,0 +1,25 @@
+let teksti = document.getElementById("pisteet")
+let pistetaulukko = []
+let yhtpist = 0
+
+function calcPoints(){
+    pistetaulukko.push(parseInt(sessionStorage.getItem("kirjoitus")))
+
+
+    pistetaulukko.forEach(peli => {
+        yhtpist += peli
+    });
+    console.log(yhtpist)
+
+    teksti.innerText = yhtpist
+}
+
+// puskekaa sessionstorageen pelin pistemäärä tälleen:
+// sessionStorage.setItem("pelin nimi", pistemäärä)
+
+// ja lisätkää tänne calcpointsiin tohon pistetaulukko.pushin alle samanlainen
+// mut silleen et tuon sessionStorage.getItem("kirjoitus") sijaan kirjoituksessa
+// lukee se nimi mitä omassa pelissä käytitte
+
+
+calcPoints()
